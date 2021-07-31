f62e0566ba62d30c09bc99ff61da4172f3a8af52
javier <javier@castroparga.com>
2021-07-31 12:09:36 +0200

    Basic sheet layouty

--- swade-spices/scripts/characterSheet.js
+++ swade-spices/scripts/characterSheet.js
@@ -1,4 +1,17 @@
 // noinspection JSFileReferences,NpmUsedModulesInstalled
 import CharacterSheet from "/systems/swade/module/sheets/official/CharacterSheet.js";
 
-export class SpicyCharacterSheet extends CharacterSheet {}
+export class SpicyCharacterSheet extends CharacterSheet {
+    // noinspection JSUnusedGlobalSymbols
+    static get defaultOptions() {
+        return {
+            ...super.defaultOptions,
+            classes: ['sheet', 'actor'],
+        };
+    }
+
+    // noinspection JSCheckFunctionSignatures
+    get template() {
+        return 'modules/swade-spices/templates/spicySheet.html';
+    }
+}

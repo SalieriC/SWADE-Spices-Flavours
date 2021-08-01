ea0d2de71e3ebfeebbb238c55bd0e098cde23645
javier <javier@castroparga.com>
2021-08-01 17:53:56 +0200

    Created themes settings.

--- swade-spices/scripts/configuration.js
+++ swade-spices/scripts/configuration.js
@@ -1,3 +1,5 @@
+import {THEMES} from "./themes.js";
+
 export const ConfigurationVariables = [
     {id: 'LeftBarBGColour', variable: '--spicy-left-bar-bg-color'},
     {id: 'TopBarColour', variable: '--spicy-top-bar-bg-color'},
@@ -17,4 +19,17 @@ export function register_settings(){
             config: true,
         });
     }
+    const theme_choices = {Custom: 'Custom'}
+    for (let theme in THEMES) {
+        theme_choices[theme] = theme
+    }
+    game.settings.register('swade-spices', 'theme', {
+        name: game.i18n.localize("SWADESPICE.Theme"),
+        hint: game.i18n.localize("SWADESPICE.ThemeHint"),
+        default: 'Pulp',
+        scope: 'world',
+        config: true,
+        type: String,
+        choices: theme_choices
+    })
 }
---
+++ swade-spices/scripts/themes.js
@@ -0,0 +1,30 @@
+// THEMES DEFINITIONS
+
+export const THEMES = {
+    // Pulp
+    'Pulp' : {
+        LeftBarBGColour: "#ffffff",
+        TopBarColour: "#000000",
+        BodyBGColour: "#114400",
+        LeftBarBGImage: "",
+        TopBarBGImage: "",
+        BodyBGImage: ""
+    },
+    'Horror': {
+        LeftBarBGColour: "#ffffff",
+        TopBarColour: "#ffffff",
+        BodyBGColour: "#ffffff",
+        LeftBarBGImage: "/modules/swade-spices/assets/img/test_background_1.jpg",
+        TopBarBGImage: "/modules/swade-spices/assets/img/test_background_2.jpg",
+        BodyBGImage: "/modules/swade-spices/assets/img/test_background_3.jpg"
+
+    },
+    'Wooden': {
+        LeftBarBGColour: "#ff2222",
+        TopBarColour: "#ff3333",
+        BodyBGColour: "#ff4444",
+        LeftBarBGImage: "",
+        TopBarBGImage: "",
+        BodyBGImage: ""
+    }
+}

a889e5f9a09489eedb768fb25e848707a5b7f963
javier <javier@castroparga.com>
2021-08-09 18:28:11 +0200

    Import/export custom settings.

--- swade-spices/scripts/configuration.js
+++ swade-spices/scripts/configuration.js
@@ -58,6 +58,12 @@ class CustomConfigForm extends FormApplication {
         return options;
     }
 
+    activateListeners(html) {
+        html.find('.spices-export').on('click', export_custom_theme)
+        html.find('.spices-import').on('click', import_custom_theme)
+        return super.activateListeners(html);
+    }
+
     getData() {
         let settings_array = []
         for (let setting of ConfigurationVariables) {
@@ -82,4 +88,55 @@ class CustomConfigForm extends FormApplication {
         }
         window.location.reload()
     }
+}
+
+
+/**
+ * Exports custom customizations to a json file.
+ */
+function export_custom_theme() {
+    let options = {}
+    for (let setting of ConfigurationVariables) {
+        options[setting.id] = game.settings.get('swade-spices', setting.id)
+    }
+    saveDataToFile(JSON.stringify(options), 'json', "customizations.json")
+}
+
+/**
+ * Import global customizations from disk
+ * @return {Promise<void>}
+ */
+async function import_custom_theme() {
+    // noinspection JSUnusedGlobalSymbols
+    new Dialog({
+      title: `Import Data: ${this.name}`,
+      content: await renderTemplate("templates/apps/import-data.html",
+          {
+            hint1: game.i18n.localize("SWADESPICE.ImportHint"),
+            hint2: game.i18n.localize("SWADESPICE.ImportWarning")}),
+      buttons: {
+        import: {
+          icon: '<i class="fas fa-file-import"></i>',
+          label: "Import",
+          callback: html => {
+            const form = html.find("form")[0];
+            if ( !form.data.files.length ) return ui.notifications.error("You did not upload a data file!");
+            readTextFromFile(form.data.files[0]).then((json) => {
+                const settings = JSON.parse(json)
+                for (let setting in settings) {
+                    game.settings.set('swade-spices', setting, settings[setting])
+                }
+                window.location.reload()
+            });
+          }
+        },
+        no: {
+          icon: '<i class="fas fa-times"></i>',
+          label: "Cancel"
+        }
+      },
+      default: "import"
+    }, {
+      width: 400
+    }).render(true);
 }

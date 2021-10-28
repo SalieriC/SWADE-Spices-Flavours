dfc84283ab6440ecbb4394564b65ed16d5141f69
javier <javier@castroparga.com>
2021-10-28 17:40:13 +0200

    Show skills list.

--- swade-spices/scripts/characterSheet.js
+++ swade-spices/scripts/characterSheet.js
@@ -18,7 +18,16 @@ export class SpicyCharacterSheet extends CharacterSheet {
 
     getData() {
         let data = super.getData()
-        let trait_tab = {left: this.create_attribute_tab(data), right: {}}
+        let items_grouped = {}
+        for (let item of this.actor.items) {
+            if (item.type in items_grouped) {
+                items_grouped[item.type].push(item)
+            } else {
+                items_grouped[item.type] = [item]
+            }
+        }
+        let trait_tab = {left: this.create_attribute_tab(data),
+                         right: this.create_skill_tab(items_grouped.skill)}
         trait_tab.left.name = 'Attributes'
         trait_tab.right.name = 'Skills'
         data.spicytabs = {'trait': trait_tab}
@@ -30,7 +39,7 @@ export class SpicyCharacterSheet extends CharacterSheet {
             strength: "SWADE.AttrStr", agility: "SWADE.AttrAgi", vigor: "SWADE.AttrVig",
             spirit: "SWADE.AttrSpr", smarts: "SWADE.AttrSma"}
         let tab = {'name': game.i18n.localize("SWADE.Attributes"),
-            items : []}
+            items: []}
         for (let attribute in data.data.data.attributes) {
             const die = data.data.data.attributes[attribute].die
             let name = attribute
@@ -39,6 +48,18 @@ export class SpicyCharacterSheet extends CharacterSheet {
             }
             name += this.create_die_string(die)
             tab.items.push({item_id: attribute, name: name})
+        }
+        return tab
+    }
+
+    create_skill_tab(skills) {
+        let tab = {'name': game.i18n.localize("SWADE.Skills"), items: []}
+        const skill_ordered = skills.sort((a, b) => a.name.localeCompare(b.name))
+        for (let skill of skill_ordered) {
+            const die = skill.data.data.die
+            let name = skill.name
+            name += this.create_die_string(die)
+            tab.items.push({item_id: skill.id, name: name})
         }
         return tab
     }

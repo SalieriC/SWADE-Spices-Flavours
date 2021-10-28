64eecefeed7dcf20a4eb4c965db5fd40b94b244b
javier <javier@castroparga.com>
2021-10-28 17:03:22 +0200

    Show attribute list.

--- swade-spices/scripts/characterSheet.js
+++ swade-spices/scripts/characterSheet.js
@@ -1,4 +1,5 @@
 // noinspection JSFileReferences,NpmUsedModulesInstalled
+/* globals game */
 import CharacterSheet from "/systems/swade/module/sheets/official/CharacterSheet.js";
 
 export class SpicyCharacterSheet extends CharacterSheet {
@@ -17,11 +18,39 @@ export class SpicyCharacterSheet extends CharacterSheet {
 
     getData() {
         let data = super.getData()
-        let trait_tab = {left: {}, right: {}}
+        let trait_tab = {left: this.create_attribute_tab(data), right: {}}
         trait_tab.left.name = 'Attributes'
         trait_tab.right.name = 'Skills'
         data.spicytabs = {'trait': trait_tab}
-        console.log(data)
         return data
+    }
+
+    create_attribute_tab(data) {
+        const ATTR_TRANSLATION_STRINGS = {
+            strength: "SWADE.AttrStr", agility: "SWADE.AttrAgi", vigor: "SWADE.AttrVig",
+            spirit: "SWADE.AttrSpr", smarts: "SWADE.AttrSma"}
+        let tab = {'name': game.i18n.localize("SWADE.Attributes"),
+            items : []}
+        for (let attribute in data.data.data.attributes) {
+            const die = data.data.data.attributes[attribute].die
+            let name = attribute
+            if (attribute in ATTR_TRANSLATION_STRINGS) {
+                name = game.i18n.localize(ATTR_TRANSLATION_STRINGS[attribute])
+            }
+            name += this.create_die_string(die)
+            tab.items.push({item_id: attribute, name: name})
+        }
+        return tab
+    }
+
+    create_die_string(die) {
+        let name = ' d' + die.sides
+        if (die.modifier) {
+            if (die.modifier > 0) {
+                name += '+'
+            }
+            name += `${die.modifier}`
+        }
+        return name
     }
 }
